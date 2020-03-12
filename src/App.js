c4c777fe29dc569dcdf24128aa832111489d99c4
Sean O'Donnell <sean.reads@gmail.com>
2020-03-11 22:01:41 -0400

    - main component fetches and displays movies with React hooks - 3001 port for react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/movies");
+      const data = await response.json();
+      const results = data.results
+      setMovies(results)
+    }
+    fetchData();
+  }, []); // a la ComponentDidMount behavior
+
+  // Reverse sort by average rating
+  const ratedMovies = movies.sort((a, b) => parseFloat(b.vote_average) - parseFloat(a.vote_average))
+
+  const rows = ratedMovies.map((movie) => {
+    return (<tr>
+              <td key={movie.id}><img alt="poster" src={'http://image.tmdb.org/t/p/w300/' + movie.backdrop_path} /></td>
+              <td key={movie.id}>{movie.title}</td>
+              <td key={movie.id}>{movie.release_date}</td>
+              <td key={movie.id}>{movie.vote_average}</td>
+            </tr>)
+  });
+
+  return(
+    <div>
+      <h1>Today's Trending Movies</h1>
+      <table>
+      <tbody>
+        <tr>
+          <td>{rows}</td><td></td>
+        </tr>
+        </tbody>
+      </table>
     </div>
-  );
+  )
 }
 
 export default App;
